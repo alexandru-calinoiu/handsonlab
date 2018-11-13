65d2c49a2e59055914dd2d9eec8e3e8ae83eb0a5
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2018-11-13 10:32:33 +0100

    Solve task2

--- src/main/kotlin/devoxx/handsonlab/tasks/Task2.kt
+++ src/main/kotlin/devoxx/handsonlab/tasks/Task2.kt
@@ -1,11 +1,19 @@
 package devoxx.handsonlab.tasks
 
+import jdk.nashorn.internal.objects.Global
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 /**
-  Implement [blockingSendDelayedMessage] function, which should do the same thing as [sendDelayedMessage], but
-  use [runBlocking] so that the function should not finish until the message has been sent. Do not use [launch].
+Implement [blockingSendDelayedMessage] function, which should do the same thing as [sendDelayedMessage], but
+use [runBlocking] so that the function should not finish until the message has been sent. Do not use [launch].
  */
 
-fun blockingSendDelayedMessage(msg: String): Unit = TODO()
+fun blockingSendDelayedMessage(msg: String): Unit =
+        runBlocking {
+            delay(2_000)
+            sendMessage(msg)
+        }
+
